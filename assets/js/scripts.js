818dd809a841fc46b682854800d26d9c692e842e
JUAN-BH <juanmita_bh@hotmail.com>
2023-05-02 17:51:46 -0500

    feat: :sparkles: Adding sections,: hero, programs, curriculum, misson and polices.

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const btnReadMore = document.querySelectorAll(".ourPrograms__btn");
+
+btnReadMore.forEach((element) => {
+  element.addEventListener("click", () => {
+    alert("MODAL PAPI");
+  });
+});
--- front-page.php
+++ front-page.php
@@ -1,5 +1,177 @@
 <?php get_header(); ?>
 
-<h1>Hello world</h1>
+<section class="heroSlider">
+    <img class="heroBanner" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/heroSlider/BANNER.png'); ?>" alt="banner">
+</section>
+
+<section id="#programs" class="ourPrograms marginContainer">
+    <h1 class="ourPrograms__title">Our Programs</h1>
+    <article class="ourPrograms__content">
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/MEDUSA.png'); ?>" alt="Medusa">
+            <p>
+                Joyful JellyFish
+                <br>
+                (6wks–12mos.)
+            </p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/DELFÍN.png'); ?>" alt="Delfin">
+            <p>
+                Dandy Dolphins
+                <br>
+                (12-24 mos.)
+            </p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/CANGREJO.png'); ?>" alt="Cangrejo">
+            <p>
+                Little Lobsters
+                <br>
+                (24-36 mos.)
+            </p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/HOMBRE_SAFARI.png'); ?>" alt="Hombre safari">
+            <p>Safari Land</p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/CARRO_CON_ANIMALES.png'); ?>" alt="Carro con animales">
+            <p>
+                Jungle Land
+                <br>
+                (3-4 yrs)
+            </p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+        <div class="ourPrograms__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/CARPA.png'); ?>" alt="Carpa">
+            <p>
+                Jungle Land
+                <br>
+                (3-4 yrs)
+            </p>
+            <button type="button" class="btn ourPrograms__btn">READ MORE</button>
+        </div>
+    </article>
+</section>
+
+<div class=bg--red>
+    <section id="curriculum" class="ourCurriculum marginContainer">
+        <h2 class="ourCurriculum__title">Our Curriculum</h2>
+        <div class="ourCurriculum__content">
+
+            <article class="ourCurriculum__content__item">
+                <img class="itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourCurriculum/NIÑO_JUGANDO.png'); ?>" alt="">
+                <div class="itemData">
+                    <img class="titleIcon" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourCurriculum/arc-pink.png'); ?>" alt="icon">
+                    <h3 class="item__title">
+                        Mother Goose Time
+                        <br>
+                        RESEARCH-BASED APPROACH TO LEARNING
+                    </h3>
+
+                    <p>
+                        The Mother Goose Time curriculum applies child development theories and brain research to a unique model of teacher support,
+                        child experience, and family engagement.
+                    </p>
+                    <p>
+                        As children construct knowledge through everyday experiences,
+                        Mother Goose Time tools help extend their learning as they explore
+                        the classroom, community, and diverse, beautiful world around them.
+                    </p>
+                </div>
+            </article>
+
+            <article class="ourCurriculum__content__item">
+                <div class="itemData">
+                    <img class="titleIcon" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourCurriculum/square-green.png'); ?>" alt="icon">
+                    <h3 class="item__title">
+                        Spanish Program
+                    </h3>
+                    <p>
+                        Our Spanish program is offered in all classrooms.
+                    </p>
+                    <p>
+                        Your child will be immersed in a natural dual language learning environment.
+                        Our qualified staff plans everyday activities in both languages.
+                        Children enjoy learning Spanish through music, games, movements and sensory activities.
+                    </p>
+                    <p>
+                        Learning a second language at an early age will benefit your child for life.
+                        Studies have shown that being bilingual improves problem-solving, memory, concentration, critical-thinking and the ability to multitask.
+                    </p>
+
+                </div>
+                <img class="itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourCurriculum/MAESTRA_CON_NIÑAS.png'); ?>" alt="Maestra con estudiantes">
+            </article>
+        </div>
+    </section>
+</div>
+
+<section id="ourMission" class="ourMission marginContainer">
+    <article class="ourMission__content__item">
+        <img class="itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourMission/NIÑA_FELIZ.png'); ?>" alt="">
+        <div class="ourMission__itemData">
+            <h2 class="ourMission__title">Our Mission</h2>
+            <img class="titleIcon" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourMission/square_yellow.png'); ?>" alt="icon">
+            <p>New Generation Daycare & Learning Center LLC offers a quality Christian learning environment for infants to school age children.<br><br>
+                We are committed to providing families with a safe and nurturing community where your children will develop self confidence and social skills, explore their own creativity, and gain an understanding of the importance of community and family.</p>
+        </div>
+    </article>
+</section>
+
+<div class="bg--yellow">
+    <section id="polices" class="ourPolicies marginContainer">
+        <article class="ourPolicies__item">
+            <img class="policesImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/polices/ABACO.png'); ?>" alt="">
+            <div class="policesData">
+                <h3 class="policesTitle">Policies & Procedures</h3>
+                <p>
+                    We offer an open door policy. An open door policy allows parents to visit their children in our facility any time they wish.
+                    However, this free access must not disrupt instructional activities and classroom routines.
+                </p>
+                <p>
+                    Notice of Nondiscrimination: New Generation Daycare and Learning Center LLC does not discriminate on the basis of race,
+                    sex, color, religion, or national origin in its dealing with students and their families.
+                </p>
+                <p>
+                    Notice of operation hours and closing changes: We reserve the right to change hours of operation and closing dates with
+                    a two week notice to parents.
+                </p>
+                <p>
+                    Complete policies and procedures outlined in welcome package.
+                </p>
+            </div>
+        </article>
+        <article class="ourPolicies__item">
+            <img class="policesImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/polices/PIRAMIDE_DE_COLORES.png'); ?>" alt="">
+            <div class="policesData">
+                <h3 class="policesTitle">Hours of Operation</h3>
+                <p>
+                    Our hours of operation are Monday through Friday from 6:30 am – 6:00 pm, 12 months a year.
+                    Early drop off and late pick up depending on demand with an additional fee.
+                </p>
+                <p>
+                    Our facility is CLOSED for the following holidays:
+                </p>
+                <ul>
+                    <li>Labor Day</li>
+                    <li>Thanksgiving (Thursday & Friday)</li>
+                    <li>Christmas Eve</li>
+                    <li>Christmas Day</li>
+                    <li>New Years Eve early release</li>
+                    <li>New Years</li>
+                    <li>Memorial Day</li>
+                    <li>Independence Day</li>
+                </ul>
+            </div>
+        </article>
+    </section>
+</div>
 
 <?php get_footer(); ?>
