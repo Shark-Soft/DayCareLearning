45668b55a5c0752ebec3f7639447c3066d09c9da
JUAN-BH <juanmita_bh@hotmail.com>
2023-05-03 11:59:54 -0500

    feat: :sparkles: Adding staff, employment, nutrition, enrollment and contact sections

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 </section>
 
 <section id="#programs" class="ourPrograms marginContainer">
-    <h1 class="ourPrograms__title">Our Programs</h1>
+    <h1 class="ourPrograms__title plpTitle">Our Programs</h1>
     <article class="ourPrograms__content">
         <div class="ourPrograms__content__item">
             <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourPrograms/MEDUSA.png'); ?>" alt="Medusa">
@@ -117,8 +117,8 @@
     <article class="ourMission__content__item">
         <img class="itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourMission/NIÑA_FELIZ.png'); ?>" alt="">
         <div class="ourMission__itemData">
-            <h2 class="ourMission__title">Our Mission</h2>
-            <img class="titleIcon" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourMission/square_yellow.png'); ?>" alt="icon">
+            <h2 class="ourMission__itemData__title plpTitle">Our Mission</h2>
+            <img class="ourMission__itemData__icon" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourMission/square_yellow.png'); ?>" alt="icon">
             <p>New Generation Daycare & Learning Center LLC offers a quality Christian learning environment for infants to school age children.<br><br>
                 We are committed to providing families with a safe and nurturing community where your children will develop self confidence and social skills, explore their own creativity, and gain an understanding of the importance of community and family.</p>
         </div>
@@ -130,7 +130,7 @@
         <article class="ourPolicies__item">
             <img class="policesImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/polices/ABACO.png'); ?>" alt="">
             <div class="policesData">
-                <h3 class="policesTitle">Policies & Procedures</h3>
+                <h3 class="policesTitle plpTitle">Policies & Procedures</h3>
                 <p>
                     We offer an open door policy. An open door policy allows parents to visit their children in our facility any time they wish.
                     However, this free access must not disrupt instructional activities and classroom routines.
@@ -150,7 +150,7 @@
         </article>
         <article class="ourPolicies__item">
             <img class="policesImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/polices/PIRAMIDE_DE_COLORES.png'); ?>" alt="">
-            <div class="policesData">
+            <div class="policesData plpTitle">
                 <h3 class="policesTitle">Hours of Operation</h3>
                 <p>
                     Our hours of operation are Monday through Friday from 6:30 am – 6:00 pm, 12 months a year.
@@ -173,5 +173,155 @@
         </article>
     </section>
 </div>
+
+<section id="#staff" class="ourStaff marginContainer">
+    <article class="ourStaff__intro">
+        <h1 class="ourStaff__intro__title plpTitle">Our Staff</h1>
+        <p class="ourStaff__intro__info">
+            Our staff is trained in child development, curriculum, classroom arrangement, age-appropriate activities, classroom management, nutrition and field trip planning.
+            Our ongoing training provides our staff with the necessary skills to create a quality learning experience for your child.
+            Your child’s day will include language development, simple math concepts, art, music, stories and much more.
+        </p>
+        <h4 class="plpTitle">We are committed to:</h4>
+    </article>
+    <article class="ourStaff__content">
+        <div class="ourStaff__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourStaff/PAISAJE.png'); ?>" alt="Paisaje">
+            <p>
+                Respect each child’s individuality and strive to foster an atmosphere of caring and trust.
+            </p>
+        </div>
+        <div class="ourStaff__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourStaff/LIBROS.png'); ?>" alt="Libros">
+            <p>
+                Work with each child according to his/her individual physical, mental and emotional
+                needs using a definite Christian emphasis in accordance with Biblical principles.
+            </p>
+        </div>
+        <div class="ourStaff__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourStaff/CORAZON.png'); ?>" alt="Corazon">
+            <p>
+                Stimulate the natural instinct of learning and transmit a love of learning.
+            </p>
+        </div>
+        <div class="ourStaff__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourStaff/FAMILIA.png'); ?>" alt="Familia">
+            <p>
+                Encourage obedience and respect for parents, staff and others.
+            </p>
+        </div>
+        <div class="ourStaff__content__item">
+            <img rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/ourStaff/BALON_DE_FUTBOL.png'); ?>" alt="Balon de futbol">
+            <p>
+                Promote kindness, sharing and getting along with others.
+            </p>
+        </div>
+    </article>
+</section>
+
+<div class="bg--green">
+    <section id="employment" class="employment marginContainer">
+        <article class="employment__content">
+            <img class="employmentImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/employment/NIÑA_JUGANDO.png'); ?>" alt="Niña jugando">
+            <div class="employment__itemData">
+                <h2 class="employment__title plpTitle">Employment</h2>
+                <p>
+                    Join our amazing team!
+                    <br>
+                    We are seeking professionals who have open availability to work.
+                </p>
+                <p>
+                    Thank you for your interest in becoming part of our family at New Generation Daycare and Learning Center LLC. To apply for current openings download and complete the application along with resume and email to:
+                    info@newgdaycare.com
+                    (In-person inquiries will not be accepted)
+                </p>
+                <h5>To Join Our Amazing Team</h5>
+            </div>
+        </article>
+    </section>
+</div>
+
+<section id="nutrition" class="nutrition marginContainer">
+    <article class="nutrition__content">
+        <div class="nutrition__containerImg"><img class="nutrition__itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/nutrition/SOL.png'); ?>" alt=""></div>
+        <div class="nutrition__itemData">
+            <h2 class="nutrition__title plpTitle">Forest Café Nutrition</h2>
+            <p>Our all inclusive delicious and nutritious meals are prepared in house daily. Your child will be provided with a healthy morning snack, a hearty lunch and an afternoon snack.</p>
+        </div>
+    </article>
+</section>
+
+<div class="bg--pink">
+    <section id="enrollment" class="enrollment marginContainer">
+        <h1 class="enrollment__title plpTitle">Enrollment application</h1>
+        <article class="employment__content">
+            <div class="employmentItem">
+                <h5>He application and admissions forms.</h5>
+                <ul>
+                    <li> - Medical Release Form (authorization)</li>
+                    <li> - Emergency Contact Form</li>
+                    <li> - Completed Enrollment Form</li>
+                    <li> - Pick Up Authorization Form</li>
+                    <li> - Record and Statement of Child’s Health</li>
+                    <li> - Tuition Contract</li>
+                    <li> - Media Release Form</li>
+                </ul>
+            </div>
+            <div class="employmentItem">
+                <h5>Other Forms</h5>
+                <ul>
+                    <li> - Toilet Training Information Form</li>
+                    <li> - Field Trip Permission Slip (transportation)</li>
+                    <li> - Disciplinary Form</li>
+                    <li> - Policy Sheet (sickness, vacation)</li>
+                    <li> - Water day other actvs</li>
+                </ul>
+            </div>
+        </article>
+        <article class="employment__footer">
+            <a href="https://drive.google.com/drive/folders/14crhrINzP4Q726TdcsbseJachiKzbpzz" target="_blank">
+                <h4 class="plpTitle">Download Forms</h4>
+            </a>
+            <p>
+                Return your completed admissions forms along with the SC Immunization Record (DHEC Form 2740).
+                Once your completed application is returned, the office staff will review and contact you with approval.
+            </p>
+        </article>
+    </section>
+</div>
+
+<section id="contact" class="contact marginContainer">
+    <h2 class="contact__title">Contact</h2>
+    <article class="contact__content">
+        <div class="contact__container">
+            <img class="contact__itemImg" rel="preload" src="<?php echo esc_url(get_template_directory_uri() . '/images/contact/TREN.png'); ?>" alt="">
+            <form action="/my-handling-form-page" method="post">
+                <ul class="inputs">
+                    <li>
+                        <!-- <label for="name">NAME:</label> -->
+                        <input type="text" id="name" name="user_name" placeholder="NAME">
+                    </li>
+                    <li>
+                        <!-- <label for="mail">EMAIL:</label> -->
+                        <input type="email" id="mail" name="user_mail" placeholder="E-mail">
+                    </li>
+                    <li>
+                        <!-- <label for="mail">EMAIL:</label> -->
+                        <input type="phone" id="phone" name="user_phone" placeholder="PHONE">
+                    </li>
+                    <li>
+                        <!-- <label for="msg">MESSAGE:</label> -->
+                        <textarea id="msg" name="user_message"></textarea>
+                    </li>
+                </ul>
+                <button class="button">Send message</button>
+            </form>
+        </div>
+        <div class="contact__itemData">SOY EL MAPA
+            <!-- <h2 class="nutrition__title">Forest Café Nutrition</h2>
+            <p>Our all inclusive delicious and nutritious meals are prepared in house daily. Your child will be provided with a healthy morning snack, a hearty lunch and an afternoon snack.</p> -->
+        </div>
+    </article>
+</section>
 
 <?php get_footer(); ?>
